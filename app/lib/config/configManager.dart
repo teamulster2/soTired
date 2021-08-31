492ca4147716cd86294f04d1aa07254fc570c9ea
mac641 <master.tww@web.de>
2021-08-31 09:33:19 +0200

    [issue/20] - Add basic config structure

---
+++ app/lib/config/configManager.dart
@@ -0,0 +1,9 @@
+import 'package:so_tired/config/serverConfig.dart';
+import 'package:so_tired/config/clientConfig.dart';
+
+class ConfigManager {
+  late final ClientConfig clientConfig;
+  late final ServerConfig serverConfig;
+
+
+}
