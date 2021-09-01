d8d2b6cd13c5051dbdf6e6da4aeade1549c927f8
Lukas Diehm <Lukas.Diehm@hs-augsburg.de>
2021-09-01 15:36:40 +0200

    [issue/18] - added local-notifications
      - add an dummy logo
      - add simple and periodic push notification

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:so_tired/config/config_manager.dart';
 import 'package:so_tired/ui/core/home/home.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:so_tired/notification.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 final ConfigManager configManager = ConfigManager();
+final Notifications notification = Notifications();
 
 void main() {
   runApp(const MyApp());
+
+  notification.initializeSetting();
+  tz.initializeTimeZones();
 
   configManager.loadDefaultConfig();
   // TODO: Use this to initialize config
@@ -25,43 +32,46 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'soTired',
-        theme: ThemeData(
-            backgroundColor: const Color(0xff0B1C46),
-            shadowColor: const Color(0xff071129),
-            primaryColor: const Color(0xff97E8D9),
-            accentColor: const Color(0xffD7AC94),
-            primaryColorDark: const Color(0xff566A9F),
-            primaryColorLight: const Color(0xffDF7CA6),
-            fontFamily: 'Roboto',
-            textTheme: const TextTheme(
-              headline1: TextStyle(
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontFamily: 'Roboto'),
-              headline2: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontFamily: 'Roboto'),
-              headline3: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontFamily: 'Roboto'),
-              bodyText1: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.white,
-                  fontFamily: 'Roboto'),
-              bodyText2: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.white,
-                  fontFamily: 'Roboto'),
-            )),
-        home: const Home(),
-      );
+  Widget build(BuildContext context) {
+    notification.showScheduleNotification(1, "soTired", "was geht ab");
+    return MaterialApp(
+      title: 'soTired',
+      theme: ThemeData(
+          backgroundColor: const Color(0xff0B1C46),
+          shadowColor: const Color(0xff071129),
+          primaryColor: const Color(0xff97E8D9),
+          accentColor: const Color(0xffD7AC94),
+          primaryColorDark: const Color(0xff566A9F),
+          primaryColorLight: const Color(0xffDF7CA6),
+          fontFamily: 'Roboto',
+          textTheme: const TextTheme(
+            headline1: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: 'Roboto'),
+            headline2: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: 'Roboto'),
+            headline3: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: 'Roboto'),
+            bodyText1: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+                fontFamily: 'Roboto'),
+            bodyText2: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+                fontFamily: 'Roboto'),
+          )),
+      home: const Home(),
+    );
+  }
 }
---
+++ app/lib/notification.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+
+class Notifications {
+  FlutterLocalNotificationsPlugin notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  /// Show a notification
+  /// The *[minutesTimer]* is the time in minutes when the push notification will be delivered.
+  /// The *[headLine]* will be showed on top in the push notification.
+  /// The *[body]* is the main msg. do you will send.
+  Future<void> showScheduleNotification(
+      int minutesTimer, String headLine, String body) async {
+    notificationsPlugin.zonedSchedule(
+        0,
+        headLine,
+        body,
+        tz.TZDateTime.now(tz.local).add(Duration(minutes: minutesTimer)),
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+              'channel id', 'channel name', 'channel description'),
+        ),
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        androidAllowWhileIdle: true);
+  }
+
+  /// Show a notification
+  /// The *[headLine]* will be showed on top in the push notification.
+  /// The *[body]* is the main msg. do you will send.
+  showSimpleNotification(String headLine, String body) async {
+    var androidDetails = AndroidNotificationDetails(
+        'id', 'channel ', 'description',
+        priority: Priority.high, importance: Importance.max);
+    var iOSDetails = IOSNotificationDetails();
+    var platformDetails =
+        new NotificationDetails(android: androidDetails, iOS: iOSDetails);
+    await notificationsPlugin.show(0, headLine, body, platformDetails,
+        payload: 'Destination Screen (Simple Notification)');
+  }
+
+  /// Periodically show a notification using the specified interval.
+  /// Give the method a RepeatInterval for example:
+  ///
+  /// <RepeatInterval.everyMinute>
+  ///
+  /// The headline and body msg. will be loaded from the config.
+  Future<void> showPeriodicNotification(RepeatInterval interval) async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+            'channel_id', 'Channel Name', 'Channel Description');
+    const NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+    await notificationsPlugin.periodicallyShow(
+        0,
+        "soTired", //TODO: load from config
+        "It's time to play with me, you tired warrior.",
+        //TODO: load from config
+        interval,
+        notificationDetails,
+        payload: 'Destination Screen(Periodic Notification)');
+  }
+
+  ///initializing the setting for Android.
+  void initializeSetting() async {
+    var initializeAndroid = AndroidInitializationSettings('logo');//TODO: add a real logo
+    var initializeSetting = InitializationSettings(android: initializeAndroid);
+    await notificationsPlugin.initialize(initializeSetting);
+  }
+}
