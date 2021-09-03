4a80b49c80c0edc19951490f2f89e060e1d315fe
Anna Gsell <annagsell@hotmail.de>
2021-09-03 11:53:42 +0200

    [issue/35] - Add application description for cobra

--- server/cmd/root.go
+++ server/cmd/root.go
@@ -31,14 +31,9 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "sotiredserver",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "soti-server",
+	Short: "server side backend of soTired apps",
+	Long:  `server side backend for data management of soTired apps, e. g. for study management.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
