0c41eb8eac9b90cfdbd5fcd354b04d4d8b2a5a1c
carinaHSA <carina.strehler@hs-augsburg.de>
2021-09-09 14:42:47 +0200

    [issue/54] - Added self-assessment
    - Added current emotional state tracker
    - Added current activity tracker

---
+++ app/lib/ui/modules/self_assessment/widgets/current_activity.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class CurrentActivity extends StatelessWidget {
+  const CurrentActivity({required this.onTap, Key? key}) : super(key: key);
+  final Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) => Container(
+      height: (MediaQuery.of(context).size.height - 81) / 2,
+      width: MediaQuery.of(context).size.width,
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Select your current activity: ',
+            style: Theme.of(context).textTheme.headline3,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 40),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Activity(
+                onTap: () => onTap(1), icon: Icons.home_rounded, text: 'home'),
+            const SizedBox(width: 40),
+            Activity(
+                onTap: () => onTap(2), icon: Icons.work_rounded, text: 'work'),
+            const SizedBox(width: 40),
+            Activity(
+                onTap: () => onTap(3),
+                icon: Icons.maps_home_work_rounded,
+                text: 'university'),
+          ]),
+          const SizedBox(height: 40),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Activity(
+                onTap: () => onTap(4),
+                icon: Icons.shopping_bag_rounded,
+                text: 'shops'),
+            const SizedBox(width: 40),
+            Activity(
+                onTap: () => onTap(5),
+                icon: Icons.people_rounded,
+                text: 'friends / family'),
+            const SizedBox(width: 40),
+            Activity(
+                onTap: () => onTap(6),
+                icon: Icons.my_location_rounded,
+                text: 'other'),
+          ]),
+        ],
+      ));
+}
+
+class Activity extends StatelessWidget {
+  const Activity(
+      {required this.icon, required this.text, required this.onTap, Key? key})
+      : super(key: key);
+  final IconData icon;
+  final String text;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+      onTap: onTap,
+      child: Column(children: <Widget>[
+        Icon(icon, color: Theme.of(context).primaryColor, size: 50),
+        const SizedBox(height: 10),
+        Text(text, style: Theme.of(context).textTheme.bodyText2)
+      ]));
+}
