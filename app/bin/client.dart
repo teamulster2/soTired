33d209f1ad32cab0afa990a49e7877b588417444
Lukas Diehm <Lukas.Diehm@hs-augsburg.de>
2021-09-09 09:59:03 +0200

    [issue/61] - added a little server/client example to test dart client

---
+++ app/bin/client.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+void main() async {
+
+  // connect to the socket server
+  final Socket socket = await Socket.connect("""
+192.168.42.20""", 50000);
+  print('Connected to: ${socket.remoteAddress.address}:${socket.remotePort}');
+
+  // listen for responses from the server
+  socket.listen(
+
+    // handle data from the server
+        (Uint8List data) {
+      final String serverResponse = String.fromCharCodes(data);
+      print('Server: $serverResponse');
+    },
+
+    // handle errors
+    onError: (error) {
+      print(error);
+      socket.destroy();
+    },
+
+    // handle server ending connection
+    onDone: () {
+      print('Server left.');
+      socket.destroy();
+    },
+  );
+
+  // send some messages to the server
+  await sendMessage(socket, 'Knock, knock.');
+  await sendMessage(socket, 'Banana');
+  await sendMessage(socket, 'Banana');
+  await sendMessage(socket, 'Banana');
+  await sendMessage(socket, 'Banana');
+  await sendMessage(socket, 'Banana');
+  await sendMessage(socket, 'Orange');
+  await sendMessage(socket, "Orange you glad I didn't say banana again?");
+}
+
+Future<void> sendMessage(Socket socket, String message) async {
+  print('Client: $message');
+  socket.write(message);
+  await Future.delayed(const Duration(seconds: 2));
+}
