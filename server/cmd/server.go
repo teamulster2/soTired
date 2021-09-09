6de0fa008a70e218329604d09537a99cd7a295d1
treilik <treilik@posteo.de>
2021-09-09 14:15:38 +0200

    [issue/63] - Add a simple http echo server

---
+++ server/cmd/server.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+func serveRun(cmd *cobra.Command, args []string) {
+	// Set routing rules
+	http.HandleFunc("/", root)
+
+	addr := fmt.Sprintf(":%s", cmd.Flag("port").Value.String())
+	fmt.Println("Start to listen on:", addr)
+	//Use the default DefaultServeMux.
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	buf, _ := io.ReadAll(r.Body)
+	io.WriteString(w, string(buf))
+}
