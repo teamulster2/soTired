2381726198686a0ac2a1bf95f530f10e8bb1921b
Lukas Diehm <Lukas.Diehm@hs-augsburg.de>
2021-09-14 11:41:30 +0200

    [issue/64] - added method to send data to the server

--- app/lib/api/client.dart
+++ app/lib/api/client.dart
@@ -16,23 +16,14 @@ Future<String> loadConfig(String url) async {
   }
 }
 
-// Future<Config> sendData() async {
-//   final http.Response response = await http.post(
-//     Uri.parse('http://192.168.1.243:50000/data'),
-//     headers: <String, String>{
-//       'Content-Type': 'data/json; charset=UTF-8',
-//     },
-//     body: 'for you, a data json file',
-//   );
-//
-// //ignore: avoid_print
-//   print('Response: ' + response.body);
-//
-//   if (response.statusCode == 200) {
-//
-//
-//     return Config.fromJson(jsonDecode(response.body));
-//   } else {
-//     throw Exception('Failed to send data.');
-//   }
-// }
+Future<void> sendData(String url, String jsonDatabase) async {
+  final http.Response response = await http.post(
+    Uri.parse(url + '/data'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonDatabase,
+  );
+//ignore: avoid_print
+  print('Response: ' + response.body);
+}
