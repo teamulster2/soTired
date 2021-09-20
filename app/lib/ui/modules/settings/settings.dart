c22310029b0bd850e22bb9233eceffb7d5d59567
carinaHSA <carina.strehler@hs-augsburg.de>
2021-09-20 17:53:03 +0200

    [issue/90] - Added title to navigation bar widget
    - Added custom title to navigation bar widget to show the title of the
    current position of the app instead of 'soTired'

--- app/lib/ui/modules/settings/settings.dart
+++ app/lib/ui/modules/settings/settings.dart
@@ -31,7 +31,7 @@ class _SettingsState extends State<Settings> {
       resizeToAvoidBottomInset: false,
       appBar: const PreferredSize(
         preferredSize: Size.fromHeight(55),
-        child: NavigationBar(),
+        child: NavigationBar(title: 'Settings'),
       ),
       // NOTE: drawer not needed now
       // drawer: const NavigationDrawer(),
