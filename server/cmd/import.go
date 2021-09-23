0701f83a97baf351912792ee84bfd56c6a032ef4
treilik <treilik@posteo.de>
2021-09-23 12:15:41 +0200

    [issue/69] - database entry creation according to client json structs

--- server/cmd/import.go
+++ server/cmd/import.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 
+	"github.com/pkg/errors"
 	"gorm.io/gorm"
 )
 
@@ -83,27 +85,26 @@ func (c clientJSON) clientJSONToDB(db *gorm.DB) error {
 	// search for existing study
 	studyDB := db.Where("study_name = ?", c.StudyName)
 	if err := studyDB.Error; err != nil {
-		return err
+		return errors.Wrap(err, fmt.Sprintf("failed to find a study with this name: %s:", c.StudyName))
 	}
 	var study Study
 	if err := studyDB.Find(&study).Error; err != nil {
-		return err
+		return errors.Wrap(err, fmt.Sprintf("failed to find a study with this name: %s:", c.StudyName))
 	}
 	// create new user for each send database since we dont want to track them
-	var newUser User
+	newUser := User{StudyID: study.ID}
 	db.Create(&newUser)
 
 	for _, pair := range createPairs(c.UserLogList, c.UserStateList) {
-		newMood := pair.userState.toMood()
-		newActivity := pair.userState.toActivity()
-		newAccessMethod := pair.userLog.toAccessMethod()
 		newSSTResult := SSTResult{SSTResultValue: pair.userLog.SpatialSpanTask}
 		newPVTResult := PVTResult{PVTResultValue: pair.userLog.PsychomotorVigilanceTask}
 
-		// FIXME error checks
-		db.Create(&newMood)
-		db.Create(&newActivity)
-		db.Create(&newAccessMethod)
+		if err := db.Create(&newSSTResult).Error; err != nil {
+			return errors.Wrap(err, "failed to crete database entry")
+		}
+		if err := db.Create(&newPVTResult).Error; err != nil {
+			return errors.Wrap(err, "failed to crete database entry")
+		}
 
 		// NOTE use the earlier timestamp
 		timestamp := pair.userLog.getTime()
@@ -113,19 +114,32 @@ func (c clientJSON) clientJSONToDB(db *gorm.DB) error {
 
 		ul := UserLog{
 			UserID:       newUser.ID,
-			Mood:         newMood,
-			Activity:     newActivity,
-			AccessMethod: newAccessMethod,
+			Mood:         pair.userState.toMood(),
+			Activity:     pair.userState.toActivity(),
+			AccessMethod: pair.userLog.toAccessMethod(),
 			SSTResultID:  newSSTResult.ID,
 			PVTResultID:  newPVTResult.ID,
 			TimeStamp:    timestamp,
 		}
-		db.Create(&ul)
+		if err := db.Create(&ul).Error; err != nil {
+			return err
+		}
 	}
-
-	studyDB.Find(&c.UserLogList)
-	studyDB.Find(&c.UserStateList)
-	studyDB.Find(&c.QuestionnaireResultList)
-
+	// return early because of Notes below
 	return nil
+
+	// FIXME wait for the client to fix the question and answer fields to string for the later block to be usefull
+	for _, jQR := range c.QuestionnaireResultList {
+		ql := QuestionnaireLog{UserID: newUser.ID}
+		db.Create(&ql)
+		// NOTE question and anserIDs are left out hear since the client send yet the wrong type: int
+		// which is a internal representation/reference of the client database from which we can not infer the correct corresponing questions and answers.
+		_ = jQR
+		// FIXME after the json field has the correct type of a string
+		// search for the correct question and answer and add them here
+		qr := QuestionnaireResult{QuestionnaireLogID: ql.ID}
+		db.Create(&qr)
+
+	}
+	panic("should not be yet reachable")
 }
